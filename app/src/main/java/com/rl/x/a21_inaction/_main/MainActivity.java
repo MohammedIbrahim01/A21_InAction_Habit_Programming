9818427a3a3c596796525c0dfdadff1c311ad3a8
X-key <imohammedibrahim01@gmail.com>
2018-07-22 15:12:53 +0200

    adding dependencies and structuring the application

--- app/src/main/java/com/rl/x/a21_inaction/_main/MainActivity.java
+++ app/src/main/java/com/rl/x/a21_inaction/_main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.rl.x.a21_inaction;
+package com.rl.x.a21_inaction._main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.rl.x.a21_inaction.R;
 
 public class MainActivity extends AppCompatActivity {
 
