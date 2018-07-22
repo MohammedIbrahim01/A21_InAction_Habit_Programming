f73c0d7aeb073521518b48c73068e07fa7ba2d3d
X-key <imohammedibrahim01@gmail.com>
2018-07-22 15:34:29 +0200

    create day zero fragment

---
+++ app/src/main/java/com/rl/x/a21_inaction/day_zero/view/DayZeroFragment.java
@@ -0,0 +1,30 @@
+package com.rl.x.a21_inaction.day_zero.view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rl.x.a21_inaction.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DayZeroFragment extends Fragment {
+
+    @BindView(R.id.expectations_recyclerView)
+    RecyclerView expectationsRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_day_zero, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+}
